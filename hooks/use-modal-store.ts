b579cc5e412d9fcf038b50cbfb27d6297063868d
Raunak0713 <raunakbhanarkar7@gmail.com>
2023-10-01 16:07:41 +0530

    Navigation

---
+++ hooks/use-modal-store.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+
+export type ModalType = "createServer";
+
+// interface ModalData {
+//     server?: Server;
+//     channel?: Channel;
+//     channelType?: ChannelType;
+//     apiUrl?: string;
+//     query?: Record<string, any>;
+// }
+
+interface ModalStore {
+    type: ModalType | null;
+    // data: ModalData;
+    isOpen: boolean;
+    onOpen: (type: ModalType/*, data?: ModalData*/) => void;
+    onClose: () => void;
+}
+
+export const useModal = create<ModalStore>((set) => ({
+    type: null,
+    // data: {},
+    isOpen: false,
+    onOpen: (type) => set({ isOpen: true, type }),
+    onClose: () => set({ type: null, isOpen: false })
+}));
